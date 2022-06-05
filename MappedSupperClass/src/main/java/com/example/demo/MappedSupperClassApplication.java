08efc752f8c1a1c8ace08ddaad5830bcd31438f8
nhuhuy2000 <nhuhuy2000@gmail.com>
2022-06-05 16:50:44 +0700

    first commit

---
+++ MappedSupperClass/src/main/java/com/example/demo/MappedSupperClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappedSupperClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappedSupperClassApplication.class, args);
+	}
+
+}
